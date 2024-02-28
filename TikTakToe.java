ff1680dbb935a376a3f9476856d60013fcea8b35
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2024-02-28 21:47:03 +0530

    improved functionality
    - identifyCell class identify the user input, update the array index with the relevant user-turn-symbol

--- TikTakToe.java
+++ TikTakToe.java
@@ -4,7 +4,7 @@ public class TikTakToe {
     public static void main(String[] args) {
 
         // initializations
-        String[][] markedCells = {
+        String[][] cells = {
                 { " ", " ", " " },
                 { " ", " ", " " },
                 { " ", " ", " " }
@@ -19,79 +19,72 @@ public class TikTakToe {
         // take user inputs
         System.out.print("User X: "); // first user input
         int userX = read.nextInt();
-        board1.identifyCell(userX);
-        int x = board1.colIndex;
-        int y = board1.rowIndex;
-
-        markedCells[x][y] = "X";
-        board1.printBoard(markedCells);
+        board1.identifyCell(cells, userX);
+        board1.printBoard(cells);
 
         System.out.print("User Y: "); // second user input
         int userY = read.nextInt();
-        board1.identifyCell(userY);
-        int x1 = board1.colIndex;
-        int y1 = board1.rowIndex;
-
-        markedCells[x1][y1] = "0";
-        board1.printBoard(markedCells);
+        board1.identifyCell(cells, userY);
+        board1.printBoard(cells);
 
     }
 }
 
 class CreateBoard {
 
-    // hold 2d array indexes
-    int colIndex, rowIndex;
+    // identify the cell number and update the array
+    void identifyCell(String[][] cells, int userInput) {
+        String symbol = " ";
+        int count = 5;
+        int x = 0;
 
-    // identify the cell number of any given input and update the array indexing
-    void identifyCell(int userInput) {
+        for (int i = 1; i < 10; i++) {
 
-        if (userInput <= 0 && userInput >= 11) {
-            userInput = 0;
-        } else {
-            switch (userInput) {
-                case 1:
-                    colIndex = 0; // 1
-                    rowIndex = 0;
-                    break;
-                case 2:
-                    colIndex = 0; // 2
-                    rowIndex = 1;
-                    break;
-                case 3:
-                    colIndex = 0; // 3
-                    rowIndex = 2;
-                    break;
-                case 4:
-                    colIndex = 1; // 4
-                    rowIndex = 0;
-                    break;
-                case 5:
-                    colIndex = 1; // 5
-                    rowIndex = 1;
-                    break;
-                case 6:
-                    colIndex = 1; // 6
-                    rowIndex = 2;
-                    break;
-                case 7:
-                    colIndex = 2; // 7
-                    rowIndex = 0;
-                    break;
-                case 8:
-                    colIndex = 2; // 8
-                    rowIndex = 1;
-                    break;
-                case 9:
-                    colIndex = 2; // 9
-                    rowIndex = 2;
-                    break;
+            // change between X and 0 depending on the user turn
+            while (x < count) {
+                if (x % 2 == 0) {
+                    symbol = "X";
+                } else {
+                    symbol = "0";
+                }
+                x++;
             }
 
-            // figure out a way to replace the switch-case with a loop.
+            if (userInput <= 0 && userInput >= 11) {
+                System.out.println("Invalid input!");
 
+            } else {
+                switch (userInput) {
+                    case 1:
+                        cells[0][0] = symbol;
+                        break;
+                    case 2:
+                        cells[1][0] = symbol;
+                        break;
+                    case 3:
+                        cells[2][0] = symbol;
+                        break;
+                    case 4:
+                        cells[0][1] = symbol;
+                        break;
+                    case 5:
+                        cells[1][1] = symbol;
+                        break;
+                    case 6:
+                        cells[2][1] = symbol;
+                        break;
+                    case 7:
+                        cells[0][2] = symbol;
+                        break;
+                    case 8:
+                        cells[1][2] = symbol;
+                        break;
+                    case 9:
+                        cells[2][2] = symbol;
+                        break;
+                }
+            }
         }
-
     }
 
     // print the tiktaktoe board with the corresponding array values
