6dda8dc2f76483e05bd41de5b1c1f1b6a64de2e3
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2024-03-02 16:19:00 +0530

    Complete game cycle
    - identifies empty and marked cells
    - identifies winner
    
    This fixes #5 and #10

--- TikTakToe.java
+++ TikTakToe.java
@@ -6,38 +6,52 @@ public class TikTakToe {
         // initializations
         String[][] cells = { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
         Scanner read = new Scanner(System.in);
-        CreateBoard board1 = new CreateBoard();
+        CreateBoard game1 = new CreateBoard();
 
         // intro prompt
         System.out.println("\nWelcome to \n  !SUPER TIK TAK TOE!\n");
 
         // take user inputs
-        System.out.print("User X: "); // first user
-        int userX = read.nextInt();
-        while (!board1.isEmptyCell(cells, userX)) {
-            System.out.println("Cell already marked. Please select an empty cell.");
-            System.out.print("User X: ");
-            userX = read.nextInt();
-        }
-        board1.identifyCell(cells, userX);
-        board1.printBoard(cells);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("User X: "); // first user
+            int userX = read.nextInt();
+            while (!game1.isEmptyCell(cells, userX)) {
+                System.out.println("Cell already marked. Please select an empty cell.");
+                System.out.print("User X: ");
+                userX = read.nextInt();
+            }
+            game1.identifyCell(cells, userX);
+            game1.printBoard(cells);
+            if (game1.hasWinner(cells)) {
+                System.out.println("User X wins!");
+                read.close();
+                return;
+            }
 
-        System.out.print("User Y: "); // second user
-        int userY = read.nextInt();
-        while (!board1.isEmptyCell(cells, userY)) {
-            System.out.println("Cell already marked. Please select an empty cell.");
-            System.out.print("User Y: ");
-            userY = read.nextInt();
+            System.out.print("User Y: "); // second user
+            int userY = read.nextInt();
+            while (!game1.isEmptyCell(cells, userY)) {
+                System.out.println("Cell already marked. Please select an empty cell.");
+                System.out.print("User Y: ");
+                userY = read.nextInt();
+            }
+            game1.identifyCell(cells, userY);
+            game1.printBoard(cells);
+            if (game1.hasWinner(cells)) {
+                System.out.println("User Y wins!");
+                read.close();
+                return;
+            }
         }
-        board1.identifyCell(cells, userY);
-        board1.printBoard(cells);
+
+        read.close();
     }
 }
 
 class CreateBoard {
 
     String symbol = " ";
-    int x = 0;
+    int symbolChanger = 0;
 
     // check if the cell is empty
     boolean isEmptyCell(String[][] cells, int userInput) {
@@ -69,7 +83,7 @@ class CreateBoard {
     void identifyCell(String[][] cells, int userInput) {
 
         // change symbol between X and 0 accordingly
-        if (x % 2 == 0) {
+        if (symbolChanger % 2 == 0) {
             symbol = "X";
         } else {
             symbol = "0";
@@ -109,7 +123,7 @@ class CreateBoard {
                     break;
             }
         }
-        x++;
+        symbolChanger++;
     }
 
     // print the tiktaktoe board with the corresponding array values
@@ -126,5 +140,35 @@ class CreateBoard {
             System.out.println("\n" + boarder);
         }
 
+    }
+
+    // check the if there is a winner
+    boolean hasWinner(String[][] cells) {
+
+        // check rows
+        for (int i = 0; i < 3; i++) {
+            if (cells[i][0].equals(cells[i][1]) && cells[i][1].equals(cells[i][2]) && !cells[i][0].equals(" ")) {
+                return true;
+            }
+        }
+
+        // check colsumns
+        for (int j = 0; j < 3; j++) {
+            if (cells[0][j].equals(cells[1][j]) && cells[1][j].equals(cells[2][j]) && !cells[0][j].equals(" ")) {
+                return true;
+            }
+        }
+
+        // check diagonals(\)
+        if (cells[0][0].equals(cells[1][1]) && cells[1][1].equals(cells[2][2]) && !cells[0][0].equals(" ")) {
+            return true;
+        }
+
+        // check diagonals(/)
+        if (cells[2][0].equals(cells[1][1]) && cells[1][1].equals(cells[0][2]) && !cells[2][0].equals(" ")) {
+            return true;
+        }
+
+        return false;
     }
 }
